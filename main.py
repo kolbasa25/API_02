bfbedecb1ee45d753c05a2f8ce1b326d453f79eb
kolbasa25 <63467154+kolbasa25@users.noreply.github.com>
2026-03-05 21:15:15 +1000

    main

--- main.py
+++ main.py
@@ -3,37 +3,40 @@ import requests
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
-
 API_SHORTEN = "https://clc.li/api/url/add"
 API_LIST = "https://clc.li/api/urls"
 
 
-def shorten_link(link):
-    r = requests.post(
+def shorten_link(token, link):
+    response = requests.post(
         API_SHORTEN,
-        headers={"Authorization": f"Bearer {TOKEN}"},
+        headers={"Authorization": f"Bearer {token}"},
         json={"url": link}
     )
-    r.raise_for_status()
-    data = r.json()
-    if data.get("error"):
-        raise requests.exceptions.HTTPError(data.get("message"))
-    return data["shorturl"]
+    response.raise_for_status()
+
+    api_response = response.json()
+
+    if api_response.get("error"):
+        raise requests.exceptions.HTTPError(api_response.get("message"))
+
+    return api_response["shorturl"]
 
 
-def count_clicks(link):
-    r = requests.get(
+def count_clicks(token, link):
+    response = requests.get(
         API_LIST,
-        headers={"Authorization": f"Bearer {TOKEN}"},
+        headers={"Authorization": f"Bearer {token}"},
         params={"short": link}
     )
-    r.raise_for_status()
-    data = r.json()
-    if data.get("error"):
-        raise requests.exceptions.HTTPError(data.get("message"))
-    return data["data"]["clicks"]
+    response.raise_for_status()
+
+    api_response = response.json()
+
+    if api_response.get("error"):
+        raise requests.exceptions.HTTPError(api_response.get("message"))
+
+    return api_response["data"]["clicks"]
 
 
 def is_bitlink(url):
@@ -41,18 +44,19 @@ def is_bitlink(url):
 
 
 def main():
+    load_dotenv()
+    api_token = os.getenv("CLC_API_TOKEN")
+
     user_input = input("Введите ссылку: ").strip()
 
     if is_bitlink(user_input):
-        clicks = count_clicks(user_input)
-        print("Количество кликов:", clicks)
+        print("Количество кликов:", count_clicks(api_token, user_input))
     else:
-        short = shorten_link(user_input)
-        print("Короткая ссылка", short)
+        print("Короткая ссылка", shorten_link(api_token, user_input))
 
 
 if __name__ == "__main__":
     try:
         main()
-    except requests.exceptions.RequestException as e:
-        print("Ошибка:", e)
+    except requests.exceptions.RequestException as error:
+        print("Ошибка:", error)
